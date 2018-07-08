53620e81469fb2a5218caa24d35bf41ef8204a66
Ahsan Fazal <ahsanfazal98@gmail.com>
2018-07-08 18:36:47 +0100

    Database connection established

--- src/index.js
+++ src/index.js
@@ -1,28 +1,44 @@
 const Hapi = require('hapi');
 const routes = require('./routes')
-
-const server = Hapi.server({
-    port: 3000,
-    host: 'localhost'
-});
-
-var redis = require("redis"),
-    client = redis.createClient();
-
-client.on("error", function (err) {
-    console.log("Error " + err);
-});
+const database = require('../.env.json')
+// Server
+const server = Hapi.server({ port: 3000, host: 'localhost' })
+// Redis
+const redis = require("redis")
+const client = redis.createClient()
+client.on("error", function (err) { console.log("Redis error: " + err) })
 
 const init = async () => {
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
-};
-
+    await server.start()
+    console.log(`Server running at: ${server.info.uri}`)
+}
+// Catch errors
 process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
-});
+    console.log(err)
+    process.exit(1)
+})
 
-init();
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(
+  database.name,
+  database.username,
+  database.password, {
+    host: database.url,
+    dialect: 'mysql',
+    operatorsAliases: false,
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
+  }
+)
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+// Init server
+init()
 routes(server)
 
